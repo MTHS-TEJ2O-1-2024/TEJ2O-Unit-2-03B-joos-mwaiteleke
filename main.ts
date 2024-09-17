23bef09f1eaabd6c4fd5c9c46b8f43588252b649
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-09-17 11:22:38 -0400

    Made it show the area and perimiter of a rectangle

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jp
+ * Created on: Sep 2024
+ * This program shows the area of a rectangle
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimenstions 5 cm & 3 cm. the perimiter would be:')
+basic.showString( ((5 + 3) * 2).toString() + ' cm')
+basic.showString('The area would be:')
+basic.showString((5 * 3).toString() + ('cm^2'))
