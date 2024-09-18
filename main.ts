1beb2233791a2418714d21f0a6cfde96e5f0c906
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-09-18 10:53:15 -0400

    Spell check

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  * This program shows the area of a rectangle
 */
 
-basic.showString('A rectangle has dimenstions 5 cm & 3 cm. the perimiter would be:')
+basic.showString('A rectangle has dimensions 5 cm & 3 cm. the perimeter would be:')
 basic.showString( ((5 + 3) * 2).toString() + ' cm')
 basic.showString('The area would be:')
 basic.showString((5 * 3).toString() + ('cm^2'))
